56713034b325ae0b68ea690fa322556114231c05
Max Torres <maxtorres487@gmail.com>
2022-08-26 18:11:35 -0400

    Define User class

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,32 @@
 package model;
 
 public class User {
+    private final String username;
+    private final String fullName;
+
+    public User(String username, String fullName) {
+        this.username = username;
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    @Override
+    public String toString() {
+        return username + ": " + fullName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User)) return false;
+        User otherUser = (User) o;
+        return username.equals(otherUser.username)
+                && fullName.equals(otherUser.fullName);
+    }
 }
