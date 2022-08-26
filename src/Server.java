7594b11628202b64cd5849ebb17d3cf7bf3c3ced
Max Torres <maxtorres487@gmail.com>
2022-08-26 15:39:59 -0400

    Move database connection method to Database.java

--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,13 @@
-import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import static util.Database.getConnection;
 
 public class Server {
-    private static final String databaseUrl = System.getenv("DATABASE_URL");
-    private static final String user = System.getenv("USER");
-    private static final String password = System.getenv("PASSWORD");
-
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection(databaseUrl, user, password);
+            Connection conn = getConnection();
             Statement statement = conn.createStatement();
             ResultSet res = statement.executeQuery("SELECT * FROM users");
             while (res.next()) {
--- src/util/Database.java
+++ src/util/Database.java
@@ -1,5 +1,30 @@
 package util;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Database {
+    private static final String databaseUrl = System.getenv("DATABASE_URL");
+    private static final String user = System.getenv("USER");
+    private static final String password = System.getenv("PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(databaseUrl, user, password);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = util.Database.getConnection();
+            createUsersTable(conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createUsersTable(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+        int res = statement.executeUpdate("DROP TABLE IF EXISTS users");
+    }
 }
