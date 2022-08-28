7dc69dda17de24148840f1390330edd65688c0c7
Max Torres <maxtorres487@gmail.com>
2022-08-28 17:02:45 -0400

    Map user read to json

--- src/api/Server.java
+++ src/api/Server.java
@@ -8,19 +8,21 @@ import static util.Database.getConnection;
 import dao.UserDao;
 import static spark.Spark.*;
 import model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Server {
     public static void main(String[] args) throws SQLException {
         Connection conn = getConnection();
         UserDao userDao = new UserDao(conn);
         port(4567);
+        ObjectMapper mapper = new ObjectMapper();
 
         path("/api", () -> {
             path("/users", () -> {
                 get("/:username", (req, res) -> {
                     String username = req.params("username");
                     User user = userDao.read(username);
-                    return user.getFullName();
+                    return mapper.writeValueAsString(user);
                 });
             });
         });
