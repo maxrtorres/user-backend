d8e023213d897153f08f26b1ff3b680390904434
Max Torres <maxtorres487@gmail.com>
2022-08-30 11:07:28 -0400

    Modify sample data

--- src/util/SampleData.java
+++ src/util/SampleData.java
@@ -8,8 +8,9 @@ import java.util.List;
 public class SampleData {
     public static List<User> sampleUsers() {
         List<User> sampleUsers = new ArrayList<>();
-        sampleUsers.add(new User("jsmith30","Joe Smith"));
-        sampleUsers.add(new User("mjohnson25", "Mary Johnson"));
+        sampleUsers.add(new User("alee3","Alex Lee"));
+        sampleUsers.add(new User("mgreen4", "Mary Green"));
+        sampleUsers.add(new User("jclark5", "Jane Clark"));
         return sampleUsers;
     }
 }
