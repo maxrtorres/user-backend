ace48888f3e7821b87b84065766d3bf3c0f39345
Max Torres <maxtorres487@gmail.com>
2022-09-02 16:04:47 -0400

    Implement reading all posts with dao

--- src/dao/PostDao.java
+++ src/dao/PostDao.java
@@ -1,6 +1,14 @@
 package dao;
 
+import exceptions.DaoException;
+import model.Post;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostDao {
     private final Connection conn;
@@ -9,4 +17,26 @@ public class PostDao {
         this.conn = conn;
     }
 
+    public List<Post> readAll() throws DaoException {
+        try {
+            String sql = "SELECT * FROM post;";
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            return getPostList(rs);
+        }
+        catch (SQLException e) {
+            throw new DaoException(e.getMessage(), e);
+        }
+    }
+
+    private List<Post> getPostList(ResultSet rs) throws SQLException {
+        List<Post> posts = new ArrayList<>();
+        while (rs.next()) {
+            posts.add(new Post(rs.getString("id"),
+                    rs.getString("author"),
+                    rs.getString("time_posted"),
+                    rs.getString("content")));
+        }
+        return posts;
+    }
 }
